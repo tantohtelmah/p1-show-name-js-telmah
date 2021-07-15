c1200b584cd5314c0075196b17252832bb33b843
tantohtelmah <82346268+tantohtelmah@users.noreply.github.com>
2021-07-16 00:32:12 +0100

    new  with extra functions

---
+++ kan1.js
@@ -0,0 +1,16 @@
+function tel() {
+    let x = document.getElementById('input_id').value;
+    document.getElementById('alert').innerHTML = 'Your input is: ' + x;
+  }
+  function tel1() {
+    let x = document.getElementById('input_id1').value;
+    document.getElementById('alert1').innerHTML = 'Your input is: ' + x;
+  }
+  function tel2() {
+    let x = document.getElementById('input_id2').value;
+    document.getElementById('alert2').innerHTML = 'Your input is: ' + x;
+  }
+  function tel3() {
+    let x = document.getElementById('input_id3').value;
+    document.getElementById('alert3').innerHTML = 'Your input is: ' + x;
+  }
